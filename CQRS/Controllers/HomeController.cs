b5d260bfad1bb9618b8eb6f5aeaf02d81b2e8cee
moynul <bappyist@gmail.com>
2022-06-28 16:35:08 +0600

    cicd

--- CQRS/Controllers/HomeController.cs
+++ CQRS/Controllers/HomeController.cs
@@ -32,6 +32,10 @@ namespace CQRS.Controllers
         {
             return View();
         }
+        public IActionResult test2()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
